ba34f0e08c96ca5221fe2779f713086c7a9251d7
Matt C <Matt@journeysoft.com>
2017-04-19 17:55:01 -0400

    prep repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require('express');
 //app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 app.use(express.static('src'));
 
-app.listen(4000, function() {
-    console.log('express is running on port 4000');
+app.listen(PORT, function() {
+    console.log('express is running on port ' + PORT);
 });
