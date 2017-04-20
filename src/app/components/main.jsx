3d92c0219dab42c0261d51ab508b7565dc8005ae
Matt C <Matt@journeysoft.com>
2017-04-20 19:32:12 -0400

    examples updated

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -3,16 +3,26 @@ const Nav = require('nav');
 
 // container component!!!!
 var Main = (props) => {
-	 
-		return (
 
-			<div>
-			 <Nav/>
-			 <h2>Main</h2>
-			 {props.children}
-			 			
+	return (
+
+		<div>
+			<Nav />
+
+			<div className="row">
+
+				<div className="columns medium-6 large-4 small-centered">
+					{props.children}
+				</div>
+
 			</div>
-			);
-}	 
+
+
+
+
+
+		</div>
+	);
+}
 
 module.exports = Main;
--- src/app/pages/examples.jsx
+++ src/app/pages/examples.jsx
@@ -1,10 +1,23 @@
 const React = require('react');
-
+const { Link } = require('react-router');
 
 var Examples = (props) =>
 	<div>
-		<h3>Examples...</h3>
-		<p>Ciao welcome... </p>
+		<h1 className="text-center">Examples</h1>
+		<p>Here are a few example locations </p>
+		<ol>
+			<li>
+				<Link to='/?location=New York'>New York</Link>
+			</li>
+			<li>
+				<Link to='/?location=Huntington Beach, California'>Huntington Beach, CA</Link>
+			</li>
+
+		</ol>
+
+
+
+
 	</div>
 
 module.exports = Examples;
